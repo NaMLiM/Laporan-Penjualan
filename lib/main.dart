3ce7000b1075d6004484c223cda46e8e53a3039f
Nafi'ul Anam <anamnafiul99@gmail.com>
2022-04-16 00:37:37 +0700

    update home

--- lib/main.dart
+++ lib/main.dart
@@ -30,13 +30,11 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        drawer: Drawer(
+          child: ListView(padding: EdgeInsets.zero, children: [
             const DrawerHeader(
               decoration: BoxDecoration(
                 color: Colors.blue,
@@ -44,15 +42,13 @@ class _HomeState extends State<Home> {
               child: Text('Drawer Header'),
             ),
             ListTile(
-              leading: Icon(Icons.warning_amber_rounded),
-              title: const Text('Tentang'),
-              onTap: () {
-              }
-            )
+                leading: Icon(Icons.warning_amber_rounded),
+                title: const Text('Tentang'),
+                onTap: () {})
           ]),
-      ),
-      body: Center(
-        child: Container(
+        ),
+        body: Center(
+            child: Container(
           padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -63,43 +59,58 @@ class _HomeState extends State<Home> {
                     children: [
                       Card(
                           child: InkWell(
-                            splashColor: Colors.blue.withAlpha(30),
-                            onTap: (){},
-                            child: SizedBox(
-                              width: 170,
-                              height: 100,
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  Icon(Icons.arrow_downward, color: Colors.green, size: 50,),
-                                  Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Text("This Month"),
-                                      Text("RP. 30.000")
-                                    ])])))),
-                      Card(
-                          child: InkWell(
                               splashColor: Colors.blue.withAlpha(30),
-                              onTap: (){},
+                              onTap: () {},
                               child: SizedBox(
                                   width: 170,
                                   height: 100,
                                   child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                    children: [
-                                      Icon(Icons.arrow_upward, color: Colors.red, size: 50,),
-                                      Column(
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        children: [
-                                          Text("This Month"),
-                                          Text("RP. 30.000")
-                                    ])])))
-                      )],
-                  ),],
-              ),],
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        Icon(
+                                          Icons.arrow_downward,
+                                          color: Colors.green,
+                                          size: 50,
+                                        ),
+                                        Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: [
+                                              Text("This Month"),
+                                              Text("RP. 30.000")
+                                            ])
+                                      ])))),
+                      Card(
+                          child: InkWell(
+                              splashColor: Colors.blue.withAlpha(30),
+                              onTap: () {},
+                              child: SizedBox(
+                                  width: 170,
+                                  height: 100,
+                                  child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        Icon(
+                                          Icons.arrow_upward,
+                                          color: Colors.red,
+                                          size: 50,
+                                        ),
+                                        Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: [
+                                              Text("This Month"),
+                                              Text("RP. 30.000")
+                                            ])
+                                      ]))))
+                    ],
+                  ),
+                ],
+              ),
+            ],
           ),
-          )
-    ));
+        )));
   }
 }
