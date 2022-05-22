dbdd67d9d09771f4ec2bb7a45b85b02d0cd95934
Nafi'ul Anam <anamnafiul99@gmail.com>
2022-05-22 16:35:20 +0700

    Move drawer to its own Class

---
+++ lib/widgets/drawer_widget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:laporan_penjualan/widgets/tentang_widget.dart';
+
+class DrawerWidget extends StatelessWidget {
+  const DrawerWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(padding: EdgeInsets.zero, children: [
+        const DrawerHeader(
+          decoration: BoxDecoration(color: Colors.blue),
+          child: Text('Laporan Penjualan'),
+        ),
+        ListTile(
+            leading: const Icon(Icons.upload_rounded),
+            title: const Text('Export to CSV'),
+            onTap: () {}),
+        ListTile(
+            leading: const Icon(Icons.warning_amber_rounded),
+            title: const Text('Tentang'),
+            onTap: () {
+              showDialog<String>(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return const TentangWidget();
+                  });
+            })
+      ]),
+    );
+  }
+}
