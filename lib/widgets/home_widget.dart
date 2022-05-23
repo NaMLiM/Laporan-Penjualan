766b7fdb547ea2580f367d4c27def0e4ad57da28
Nafi'ul Anam <anamnafiul99@gmail.com>
2022-05-23 18:11:38 +0700

    Update home_widget.dart

--- lib/widgets/home_widget.dart
+++ lib/widgets/home_widget.dart
@@ -31,109 +31,111 @@ class _HomeState extends State<Home> {
         appBar: AppBar(title: Text(widget.title)),
         drawer: const DrawerWidget(),
         body: Center(
+            child: Column(children: [
+          Container(
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor,
+            borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(25),
+                bottomRight: Radius.circular(25))),
+            child: Row(children: [
+              Expanded(
+              child: Card(
+                  child: InkWell(
+                      splashColor: Colors.blue.withAlpha(30),
+                      onTap: () {},
+                      child: SizedBox(
+                          height: 100,
+                          child: Row(
+                              mainAxisAlignment:
+                                  MainAxisAlignment.spaceEvenly,
+                              children: [
+                                const Icon(Icons.arrow_downward,
+                                    color: Colors.green, size: 50),
+                                Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.center,
+                                    children: [
+                                      Text("This Month"),
+                                      Text("RP. 30.000")
+                                    ])
+                              ]))))),
+              Expanded(
+              child: Card(
+                  child: InkWell(
+                      splashColor: Colors.blue.withAlpha(30),
+                      onTap: () {},
+                      child: SizedBox(
+                          height: 100,
+                          child: Row(
+                              mainAxisAlignment:
+                                  MainAxisAlignment.spaceEvenly,
+                              children: [
+                                const Icon(Icons.arrow_upward,
+                                    color: Colors.red, size: 50),
+                                Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.center,
+                                    children: [
+                                      Text("This Month"),
+                                      Text("RP. 30.000")
+                                    ])
+                              ])))))
+            ]),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
             child: Container(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-                child: Column(children: [
-                  Row(children: [
-                    Expanded(
-                        child: Card(
-                            child: InkWell(
-                                splashColor: Colors.blue.withAlpha(30),
-                                onTap: () {},
-                                child: SizedBox(
-                                    height: 100,
-                                    child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceEvenly,
-                                        children: [
-                                          const Icon(Icons.arrow_downward,
-                                              color: Colors.green, size: 50),
-                                          Column(
-                                              mainAxisAlignment:
-                                                  MainAxisAlignment.center,
-                                              children: [
-                                                Text("This Month"),
-                                                Text("RP. 30.000")
-                                              ])
-                                        ]))))),
-                    Expanded(
-                        child: Card(
-                            child: InkWell(
-                                splashColor: Colors.blue.withAlpha(30),
-                                onTap: () {},
-                                child: SizedBox(
-                                    height: 100,
-                                    child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceEvenly,
-                                        children: [
-                                          const Icon(Icons.arrow_upward,
-                                              color: Colors.red, size: 50),
-                                          Column(
-                                              mainAxisAlignment:
-                                                  MainAxisAlignment.center,
-                                              children: [
-                                                Text("This Month"),
-                                                Text("RP. 30.000")
-                                              ])
-                                        ])))))
-                  ]),
-                  Container(
-                      padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
-                      child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            const Text("Laporan Bulan Ini"),
-                            IconButton(
-                                onPressed: () {
-                                  setState(() {});
-                                },
-                                icon: const Icon(Icons.refresh))
-                          ])),
-                  Expanded(
-                      child: FutureBuilder(
-                          future: handler.retrieveData(),
-                          builder: (BuildContext context,
-                              AsyncSnapshot<List<Report>> snapshot) {
-                            if (snapshot.hasData) {
-                              return ListView.builder(
-                                  itemCount: snapshot.data?.length,
-                                  itemBuilder:
-                                      (BuildContext context, int index) {
-                                    return Card(
-                                        child: InkWell(
-                                            splashColor:
-                                                Colors.blue.withAlpha(30),
-                                            onTap: () {},
-                                            child: ListTile(
-                                                title: Text(snapshot
-                                                    .data![index].status),
-                                                subtitle: Text(
-                                                    snapshot.data![index].date),
-                                                leading: snapshot.data![index].status ==
-                                                        "Uang Masuk"
-                                                    ? const CircleAvatar(
-                                                        backgroundColor:
-                                                            Colors.lightBlue,
-                                                        child:
-                                                            Icon(Icons.arrow_downward,
-                                                                color: Colors
-                                                                    .greenAccent))
-                                                    : const CircleAvatar(
-                                                        backgroundColor:
-                                                            Colors.lightBlue,
-                                                        child: Icon(
-                                                            Icons.arrow_upward,
-                                                            color: Colors.redAccent)),
-                                                trailing: Text("Rp." + snapshot.data![index].nominal.toString()))));
-                                  });
-                            } else {
-                              return const Center(
-                                  child: CircularProgressIndicator());
-                            }
-                          }))
-                ]))),
+            padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text("Laporan Bulan Ini"),
+                  IconButton(
+                      onPressed: () {
+                        setState(() {});
+                      },
+                      icon: const Icon(Icons.refresh))
+                ])),
+          ),
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+            child: FutureBuilder(
+            future: handler.retrieveData(),
+            builder: (BuildContext context,
+                AsyncSnapshot<List<Report>> snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                    itemCount: snapshot.data?.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Card(
+                          child: InkWell(
+                              splashColor: Colors.blue.withAlpha(30),
+                              onTap: () {},
+                              child: ListTile(
+                                  title: Text(snapshot.data![index].status),
+                                  subtitle:
+                                      Text(snapshot.data![index].date),
+                                  leading: snapshot.data![index].status ==
+                                          "Uang Masuk"
+                                      ? const CircleAvatar(
+                                          backgroundColor: Colors.lightBlue,
+                                          child: Icon(Icons.arrow_downward,
+                                              color: Colors.greenAccent))
+                                      : const CircleAvatar(
+                                          backgroundColor: Colors.lightBlue,
+                                          child: Icon(Icons.arrow_upward,
+                                              color: Colors.redAccent)),
+                                  trailing: Text("Rp.${snapshot.data![index].nominal}"))));
+                    });
+              } else {
+                return const Center(child: CircularProgressIndicator());
+              }
+            }),
+          ))
+        ])),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             showDialog(
