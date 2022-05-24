7ac8a72cc6e830f57413a5c510932a94a86ad1b0
Nafi'ul Anam <anamnafiul99@gmail.com>
2022-05-24 14:47:44 +0700

    Add update/delete Popup

--- lib/widgets/home_widget.dart
+++ lib/widgets/home_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:laporan_penjualan/widgets/update_laporan_widget.dart';
 
 import '../models/report_model.dart';
 import '../services/db_handler.dart';
@@ -35,105 +36,121 @@ class _HomeState extends State<Home> {
           Container(
             padding: const EdgeInsets.all(20),
             decoration: BoxDecoration(
-            color: Theme.of(context).primaryColor,
-            borderRadius: const BorderRadius.only(
-                bottomLeft: Radius.circular(25),
-                bottomRight: Radius.circular(25))),
+                color: Theme.of(context).primaryColor,
+                borderRadius: const BorderRadius.only(
+                    bottomLeft: Radius.circular(25),
+                    bottomRight: Radius.circular(25))),
             child: Row(children: [
               Expanded(
-              child: Card(
-                  child: InkWell(
-                      splashColor: Colors.blue.withAlpha(30),
-                      onTap: () {},
-                      child: SizedBox(
-                          height: 100,
-                          child: Row(
-                              mainAxisAlignment:
-                                  MainAxisAlignment.spaceEvenly,
-                              children: [
-                                const Icon(Icons.arrow_downward,
-                                    color: Colors.green, size: 50),
-                                Column(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.center,
-                                    children: [
-                                      Text("This Month"),
-                                      Text("RP. 30.000")
-                                    ])
-                              ]))))),
+                  child: Card(
+                      child: InkWell(
+                          splashColor: Colors.blue.withAlpha(30),
+                          onTap: () {},
+                          child: SizedBox(
+                              height: 100,
+                              child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    const Icon(Icons.arrow_downward,
+                                        color: Colors.green, size: 50),
+                                    Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Text("This Month"),
+                                          Text("RP. 30.000")
+                                        ])
+                                  ]))))),
               Expanded(
-              child: Card(
-                  child: InkWell(
-                      splashColor: Colors.blue.withAlpha(30),
-                      onTap: () {},
-                      child: SizedBox(
-                          height: 100,
-                          child: Row(
-                              mainAxisAlignment:
-                                  MainAxisAlignment.spaceEvenly,
-                              children: [
-                                const Icon(Icons.arrow_upward,
-                                    color: Colors.red, size: 50),
-                                Column(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.center,
-                                    children: [
-                                      Text("This Month"),
-                                      Text("RP. 30.000")
-                                    ])
-                              ])))))
+                  child: Card(
+                      child: InkWell(
+                          splashColor: Colors.blue.withAlpha(30),
+                          onTap: () {},
+                          child: SizedBox(
+                              height: 100,
+                              child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    const Icon(Icons.arrow_upward,
+                                        color: Colors.red, size: 50),
+                                    Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Text("This Month"),
+                                          Text("RP. 30.000")
+                                        ])
+                                  ])))))
             ]),
           ),
           Padding(
             padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
             child: Container(
-            padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text("Laporan Bulan Ini"),
-                  IconButton(
-                      onPressed: () {
-                        setState(() {});
-                      },
-                      icon: const Icon(Icons.refresh))
-                ])),
+                padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text("Laporan Bulan Ini"),
+                      IconButton(
+                          onPressed: () {
+                            setState(() {});
+                          },
+                          icon: const Icon(Icons.refresh))
+                    ])),
           ),
           Expanded(
               child: Padding(
             padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
             child: FutureBuilder(
-            future: handler.retrieveData(),
-            builder: (BuildContext context,
-                AsyncSnapshot<List<Report>> snapshot) {
-              if (snapshot.hasData) {
-                return ListView.builder(
-                    itemCount: snapshot.data?.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Card(
-                          child: InkWell(
-                              splashColor: Colors.blue.withAlpha(30),
-                              onTap: () {},
-                              child: ListTile(
-                                  title: Text(snapshot.data![index].status),
-                                  subtitle:
-                                      Text(snapshot.data![index].date),
-                                  leading: snapshot.data![index].status ==
-                                          "Uang Masuk"
-                                      ? const CircleAvatar(
-                                          backgroundColor: Colors.lightBlue,
-                                          child: Icon(Icons.arrow_downward,
-                                              color: Colors.greenAccent))
-                                      : const CircleAvatar(
-                                          backgroundColor: Colors.lightBlue,
-                                          child: Icon(Icons.arrow_upward,
-                                              color: Colors.redAccent)),
-                                  trailing: Text("Rp.${snapshot.data![index].nominal}"))));
-                    });
-              } else {
-                return const Center(child: CircularProgressIndicator());
-              }
-            }),
+                future: handler.retrieveData(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<List<Report>> snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                        itemCount: snapshot.data?.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Card(
+                              child: InkWell(
+                                  splashColor: Colors.blue.withAlpha(30),
+                                  onLongPress: () {
+                                    Navigator.push(
+                                            context,
+                                            DialogRoute(
+                                                builder: (context) =>
+                                                    UpdateLaporanWidget(
+                                                      id: snapshot
+                                                          .data![index].id,
+                                                      status: snapshot
+                                                          .data![index].status,
+                                                      date: snapshot
+                                                          .data![index].date,
+                                                    ),
+                                                context: context))
+                                        .then((value) => setState(() {}));
+                                  },
+                                  child: ListTile(
+                                      title: Text(snapshot.data![index].status),
+                                      subtitle:
+                                          Text(snapshot.data![index].date),
+                                      leading: snapshot.data![index].status ==
+                                              "Uang Masuk"
+                                          ? const CircleAvatar(
+                                              backgroundColor: Colors.lightBlue,
+                                              child: Icon(Icons.arrow_downward,
+                                                  color: Colors.greenAccent))
+                                          : const CircleAvatar(
+                                              backgroundColor: Colors.lightBlue,
+                                              child: Icon(Icons.arrow_upward,
+                                                  color: Colors.redAccent)),
+                                      trailing: Text(
+                                          "Rp.${snapshot.data![index].nominal}"))));
+                        });
+                  } else {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                }),
           ))
         ])),
         floatingActionButton: FloatingActionButton(
---
+++ lib/widgets/update_laporan_widget.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+
+import '../models/report_model.dart';
+import '../services/db_handler.dart';
+
+class UpdateLaporanWidget extends StatefulWidget {
+  const UpdateLaporanWidget(
+      {Key? key, required this.id, required this.status, required this.date})
+      : super(key: key);
+
+  final int? id;
+  final String status;
+  final String date;
+
+  @override
+  State<StatefulWidget> createState() {
+    return _UpdateLaporanWidget();
+  }
+}
+
+enum AddToggle { masuk, keluar, empty }
+
+class _UpdateLaporanWidget extends State<UpdateLaporanWidget> {
+  AddToggle? isSelected = AddToggle.empty;
+  var textController = TextEditingController();
+  var check = true;
+
+  @override
+  Widget build(BuildContext context) {
+    if (check == true) {
+      if (widget.status == "Uang Masuk") {
+        isSelected = AddToggle.masuk;
+      } else {
+        isSelected = AddToggle.keluar;
+      }
+      check = false;
+    }
+    return AlertDialog(
+      title: const Text("Update Laporan"),
+      content: Form(
+          child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text("ID ${widget.id}"),
+          TextFormField(
+            controller: textController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+                labelText: "Nominal", border: UnderlineInputBorder()),
+          ),
+          const SizedBox(height: 10),
+          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Expanded(
+                child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isSelected = AddToggle.masuk;
+                      });
+                    },
+                    child: Card(
+                        color: isSelected == AddToggle.masuk
+                            ? Colors.lightBlueAccent
+                            : Colors.white,
+                        child: SizedBox(
+                            height: 70,
+                            child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Container(
+                                      decoration: BoxDecoration(
+                                          color: Colors.blue,
+                                          borderRadius:
+                                              BorderRadius.circular(50.0)),
+                                      child: const Icon(Icons.arrow_downward,
+                                          color: Colors.greenAccent)),
+                                  Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: const [Text("Masuk")])
+                                ]))))),
+            Expanded(
+                child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isSelected = AddToggle.keluar;
+                      });
+                    },
+                    child: Card(
+                        color: isSelected == AddToggle.keluar
+                            ? Colors.lightBlueAccent
+                            : Colors.white,
+                        child: SizedBox(
+                            height: 70,
+                            child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Container(
+                                      decoration: BoxDecoration(
+                                          color: Colors.blue,
+                                          borderRadius:
+                                              BorderRadius.circular(50.0)),
+                                      child: const Icon(Icons.arrow_upward,
+                                          color: Colors.redAccent)),
+                                  Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: const [
+                                        Text("Keluar"),
+                                      ])
+                                ]))))),
+          ])
+        ],
+      )),
+      actions: [
+        TextButton(
+            onPressed: () {
+              DatabaseHandler().deleteData(widget.id);
+              Navigator.pop(context);
+            },
+            child: const Text("Delete")),
+        TextButton(
+            onPressed: () {
+              var status = isSelected.toString();
+              isSelected == AddToggle.masuk
+                  ? status = "Uang Masuk"
+                  : status = "Uang Keluar";
+              Report data = Report(
+                  id: widget.id,
+                  nominal: int.parse(textController.text),
+                  date: widget.date,
+                  status: status);
+              DatabaseHandler().updateData(data);
+              Navigator.pop(context);
+            },
+            child: const Text("Update"))
+      ],
+    );
+  }
+}
